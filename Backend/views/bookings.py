0dc2f8ad275d6e95042044d1d8690ed5bee0baef
qurriahSam <mugirethe1@gmail.com>
2025-02-03 13:10:16 +0300

    refactor: change code indentation to fix Assertion error

--- Backend/views/bookings.py
+++ Backend/views/bookings.py
@@ -2,11 +2,11 @@ from flask import jsonify, request, Blueprint
 from models import db, User, Booking, Service
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
-bookings_bp= Blueprint('bookings_bp', __name__)
+bookings_bp = Blueprint('bookings_bp', __name__)
 
-#create
+
 @bookings_bp.route('/bookings', methods=['POST'])
-@jwt_required
+@jwt_required()
 def create_booking():
     data = request.get_json()
     current_user_id = get_jwt_identity()
@@ -20,38 +20,42 @@ def create_booking():
 
     if not check_service_id:
         return jsonify({"error": "Service not found"}), 404
-    
+
     else:
         new_booking = Booking(service_type, service_id=current_user_id, date_time=date_time, location=location)
         db.session.add(new_booking)
         db.session.commit()
         return jsonify({"message": "Booking created successfully"}), 201
-    
-    #read - Get all bookings
-    @bookings_bp.route('/bookings', methods=['GET'])
-    @jwt_required()
-    def get_all_bookings():
-        current_user_id = get_jwt_identity()
 
-        bookings = Booking.query.filter_by(service_id= current_user_id)
-        
-        booking_list = [
-            {
+
+@bookings_bp.route('/bookings', methods=['GET'])
+@jwt_required()
+def get_all_bookings():
+    current_user_id = get_jwt_identity()
+
+    bookings = Booking.query.filter_by(service_id=current_user_id)
+
+    booking_list = [
+        {
             'id': booking.id,
             'service_type': booking.service_type,
             'service_id': booking.service_id,
-            'date_time' : booking.date_time,
+            'date_time': booking.date_time,
             'location': booking.location,
-            'user': {'id': booking.user_id, 'username': booking.user.username, 'email': booking.user.email},
-            
-            } for booking in bookings
-        ]
-        return jsonify(booking_list), 200
+            'user': {
+                'id': booking.user_id,
+                'username': booking.user.username,
+                'email': booking.user.email
+            },
+        }
+        for booking in bookings
+    ]
+
+    return jsonify(booking_list), 200
 
 
-#Read - Get Booking by id
 @bookings_bp.route('/bookings/<int:booking_id>', methods=['GET'])
-@jwt_required
+@jwt_required()
 def get_booking(booking_id):
     current_user_id = get_jwt_identity()
 
@@ -69,14 +73,14 @@ def get_booking(booking_id):
     else:
         return jsonify({"error": "Booking not found"}), 404
 
-#Update
+
 @bookings_bp.route('/bookings/<int:booking_id>', methods=['PUT'])
-@jwt_required
+@jwt_required()
 def update_booking(booking_id):
     current_user_id = get_jwt_identity()
-
     data = request.get_json()
-    booking = Booking.query.get(id=booking_id)
+
+    booking = Booking.query.get(booking_id)
 
     if booking and booking.user_id == current_user_id:
         booking.service_type = data.get('service_type', booking.service_type)
@@ -87,14 +91,16 @@ def update_booking(booking_id):
         check_booking_id = User.query.get(booking_id)
 
         if check_booking_id:
-            return jsonify({"error": "User with this id already exists"}), 400
+            return jsonify({"error": "User with this ID already exists"}), 400
 
-            db.session.commit()
-            return jsonify({"message": "Booking updated successfully"}), 200
+        db.session.commit()
+        return jsonify({"message": "Booking updated successfully"}), 200
 
-#Delete
+    return jsonify({"error": "Booking not found"}), 404
+
+
 @bookings_bp.route('/bookings/<int:booking_id>', methods=['DELETE'])
-@jwt_required
+@jwt_required()
 def delete_booking(booking_id):
     current_user_id = get_jwt_identity()
 
